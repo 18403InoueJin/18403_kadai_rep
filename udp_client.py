dd247bd30e1eea8c517db1e8214a116833d7388b
inoue <18403@toba-cmt.ac.jp>
2021-11-10 15:31:28 +0900

    メッセージを変更しました

--- udp_client.py
+++ udp_client.py
@@ -1,4 +1,4 @@
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as hensuu:
-    hensuu.sendto(b'Hello UDPPP', ('127.0.0.1', 50007))
+    hensuu.sendto(b'Hello UDP world', ('127.0.0.1', 50007))
